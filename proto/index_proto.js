3dbc72a80f18da679ffa8008d0ef798e33f02c2f
alexander29600 <alexandre.rouzault@eleves.ecmorlaix.fr>
2025-01-31 11:13:40 +0100

    Première version du site

---
+++ proto/index_proto.js
@@ -0,0 +1,10 @@
+function afficherListe(){
+    const liste = document.getElementById('nouvelleListe');
+    liste.innerHTML = `
+        <li><a href="https://www.flashscore.com/match/88EIxTGr/#/match-summary/match-summary" target="_blank">Rennes vs Monaco</a></li>
+        <li><a href="https://www.flashscore.com/match/vJ50M7W1/#/match-summary/match-summary" target="_blank">Rennes vs Brest</a></li>
+        <li><a href="https://www.flashscore.com/match/UJKMKfec/#/match-summary/match-summary" target="_blank">Rennes vs OM<a/></li>
+        <li><a href="https://www.flashscore.com/match/vqy8B7je/#/match-summary/match-summary" target="_blank">Rennes vs Troyes</a></li>
+        <li><a href="https://www.flashscore.com/match/naL2UPCm/#/match-summary/match-summary" target="_blank">Rennes vs Nice<a/></li>
+    `;
+}
